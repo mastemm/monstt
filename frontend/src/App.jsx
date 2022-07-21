5a84d3d282e6cf366c5654942898446320c33943
MassimaMao <mastem@msn.com>
2022-07-21 11:48:32 +0200

    2nd commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Home from "@pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
